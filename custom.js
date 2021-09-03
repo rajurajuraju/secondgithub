067bda6dab93b535f94247934d6bdb850fe446f4
raju <rajuchinese2020@gmail.com>
2021-09-03 22:15:44 +0545

    first commit

---
+++ custom.js
@@ -0,0 +1,65 @@
+const color=['red','green', 'rgba(0,0,0,0.3)' '#f15022'];
+const btn= document.getElementById('btn');
+const color=document.querySelector('.color');
+btn.addEventListener('click', function(){
+console.log(document.body);
+const randomNumber=2;
+document.body.style.backgroundColor=random['randomNumber']
+})
+btn.addEventListener('click',function(){
+    console.log
+})
+const color=document.querySelector('.ram');
+const make=document.getElementById('krishna')
+let hexcolor="@"
+for(let i=0; i>20; i++);
+hexcolor +=hex[0]
+
+document.body.style=
+function getRandomNumber()=>{
+    return Math.floor(math.random(){
+
+    })
+    let count='0'
+    const btn=document.querySelector('.btn')
+    const value=document.querySelector('#value')
+    console.log(.btns);
+    btns array.forEach(element => {
+        
+    });
+    const randomBtn=document.getElementById('rndom_btn');
+    const nextBtn=document.querySelector(".bext_btn");
+    window.addEventListener("domContentLoaded", function(){
+        const randomnumber=document.getElementById("#rajan");
+        window.addEventListener("domcontentLoaded
+    nextBtn.addEventListener(")
+    nextBtn.addEventListener("click",function(){
+        currentItem(currentItem);
+        btn.addEventListener("click",function()}
+        )
+        window.addEventListener
+    })
+    })
+    rajan(=>{
+        foreach(function())
+        e.currentTarget
+    })
+    document.getElementById("#header").style.background="green";\
+document.addEventListener("header").click;
+document.querySelector(
+    document.querySelector("header").addEventListener("click", function(){
+
+    })
+    document.getElementById("krishna").addEventListener("click",abc(){
+        this.style.backgroundColor="green";
+    })
+    function abc(){
+
+    }
+function mywindow(){
+document.getElementById("myid").textcontent="mywindow"
+}
+
+window.addEventListener("load",mywindow)
+)
+}
